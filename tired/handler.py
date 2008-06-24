b94a97f3e4c45f638f210761440cf2b491b3357c
Mihai Ibanescu <https://issues.rpath.com/>
2008-06-24 14:44:34 -0400

    First attempt to split the request

--- tired/handler.py
+++ tired/handler.py
@@ -4,19 +4,125 @@
 
 import BaseHTTPServer
 
+class BaseRequest(object):
+    __slots__ = [ '_headersSent' ]
+    BUFFER_SIZE = 16384
+
+    def __init__(self):
+        self._headersSent = False
+
+    def write(self, data):
+        self._sendHeaders()
+        self._write(data)
+
+    def writeFromFile(self, fileObj):
+        self._sendHeaders()
+        while 1:
+            buf = fileObj.read(self.BUFFER_SIZE)
+            if not buf:
+                break
+            self._write(buf)
+
+    def _sendHeaders(self):
+        "Send headers"
+        if self._headersSent:
+            return
+        for key, val in self._iterHeadersOut():
+            if not isinstance(val, list):
+                val = [ val ]
+            for v in val:
+                self._sendHeader(key, v)
+        self._endHeaders(self)
+        self._headersSent = True
+
+    #{ Methods to be redefined in subclasses
+    def addHeader(self, key, value):
+        "Add a single header value"
+
+    def addHeaders(self, key, values):
+        "Add a multi-valued header"
+
+    def getAbsoluteURI(self):
+        "Return the absolute URI for this request"
+
+    def getRelativeURI(self):
+        "Return the relative URI for this request"
+
+    def _read(self, amt = None):
+        "Called when reading the request body"
+
+    def _write(self, data):
+        "Called when writing the response body"
+
+    def _sendHeader(self, key, value):
+        "Called when sending a header"
+
+    def _endHeaders(self):
+        "Called when done sending the headers"
+
+    def _iterHeadersOut(self):
+        "Iterate over the outgoing headers"
+    #}
+
+class StandaloneRequest(BaseRequest):
+    __slots__ = [ '_headersOut', '_req', '_read', '_write',
+                  '_sendHeader', '_endHeaders' ]
+    def __init__(self, req):
+        BaseRequest.__init__(self)
+        self._headersOut = {}
+        self._req = req
+        self._read = self._req.rfile.read
+        self._write = self._req.wfile.write
+        self._sendHeader = self._req.send_header
+        self._endHeaders = self._req.end_headers
+
+    def getRelativeURI(self):
+        return self._req.path
+
+    def getAbsoluteURI(self):
+        hostport = self._req.host
+        if self._req.port != 80 and ':' not in hostport:
+            hostport = "%s:%s" % (self._req.host, self._req.port)
+        return "http://%s%s" % (hostport, self._req.path)
+
+    def _iterHeadersOut(self):
+        for key, val in self._headersOut.items():
+            if isinstance(val, list):
+                for v in val:
+                    yield key, v
+            else:
+                yield key, val
+
+class Response(object):
+    __slots__ = [ '_headersOut', '_data', '_file' ]
+    def __init__(self, headers = None, data = None, fileObj = None)
+        self._headersOut = {}
+        for k, v in headers:
+            if isinstance(v, list):
+                self.addHeaders(k, v)
+            else:
+                self.addHeader(k, v)
+        if data:
+            self._data = data
+        elif fileObj:
+            self._file = fileObj
+        else:
+            assert False, "no data present"
+
+    def addHeader(self, key, value):
+        self._headersOut[key] = str(value)
+
+    def addHeaders(self, key, values):
+        assert(isinstance(values, list))
+        self._headersOut[key] = [ str(x) for x in values ]
+
 class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
     def log_message(self, *args, **kwargs):
         pass
 
     def do_GET(self):
-        self._validateHeaders()
+        req = self._validateHeaders()
         self._auth()
-
-        hostport = self.host
-        if self.port != 80 and ':' not in hostport:
-            hostport = "%s:%s" % (self.host, self.port)
-        prefix = "http://%s/%s/clouds/ec2/images/" % (hostport,
-                self.server.toplevel)
 
         if self.path == '/crossdomain.xml':
             return self.serveCrossDomainFile()
@@ -29,15 +135,15 @@ class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
 
     def do_PUT(self):
-        self._validateHeaders()
+        req = self._validateHeaders()
         self._auth()
 
     def do_POST(self):
-        self._validateHeaders()
+        req = self._validateHeaders()
         self._auth()
 
     def do_DELETE(self):
-        self._validateHeaders()
+        req = self._validateHeaders()
         self._auth()
 
     def _validateHeaders(self):
@@ -48,6 +154,8 @@ class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
 
         self.host = self.headers['Host']
         self.port = self.server.server_port
+        req = StandaloneRequest(self)
+        return req
 
     def _auth(self):
         pass
