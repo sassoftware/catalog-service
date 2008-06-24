8eb42497d4e8efb40ca07ad49e84f28e79e1af47
Mihai Ibanescu <https://issues.rpath.com/>
2008-06-24 18:19:35 -0400

    Added ability to store blobs under the user's name

--- tired/handler.py
+++ tired/handler.py
@@ -2,9 +2,42 @@
 # Copyright (c) 2008 rPath, Inc.
 #
 
+import base64
 import BaseHTTPServer
+import os
+
+import config
+import storage
+
+class StorageConfig(config.BaseConfig):
+    def __init__(self, storagePath):
+        config.BaseConfig.__init__(self)
+        self.storagePath = storagePath
 
 class BaseRequest(object):
+    __slots__ = [ '_userId', '_password' ]
+    def __init__(self):
+        self._userId = None
+        self._password = None
+
+    def setUser(self, user):
+        self._userId = user
+
+    def getUser(self):
+        return self._userId
+
+    def setPassword(self, password):
+        self._password = password
+
+    def getPassword(self):
+        return self._password
+
+    def getContentLength(self):
+        cl = self.getHeader('Content-Length')
+        if cl is not None:
+            return int(cl)
+        return None
+
     #{ Methods to be redefined in subclasses
     def getAbsoluteURI(self):
         "Return the absolute URI for this request"
@@ -12,16 +45,19 @@ class BaseRequest(object):
     def getRelativeURI(self):
         "Return the relative URI for this request"
 
-    def _read(self, amt = None):
+    def getHeader(self, key):
+        "Return a specific header"
+
+    def read(self, amt = None):
         "Called when reading the request body"
     #}
 
 class StandaloneRequest(BaseRequest):
-    __slots__ = [ '_req', '_read' ]
+    __slots__ = [ '_req', 'read' ]
     def __init__(self, req):
         BaseRequest.__init__(self)
         self._req = req
-        self._read = self._req.rfile.read
+        self.read = self._req.rfile.read
 
     def getRelativeURI(self):
         return self._req.path
@@ -31,6 +67,9 @@ class StandaloneRequest(BaseRequest):
         if self._req.port != 80 and ':' not in hostport:
             hostport = "%s:%s" % (self._req.host, self._req.port)
         return "http://%s%s" % (hostport, self._req.path)
+
+    def getHeader(self, key):
+        return self._req.headers.get(key, None)
 
 class Response(object):
     __slots__ = [ '_headersOut', '_data', '_file' ]
@@ -93,34 +132,55 @@ class Response(object):
                 yield key, val
 
 class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
+    toplevel = 'TOPLEVEL'
+    storageConfig = StorageConfig(storagePath = "storage")
+
     def log_message(self, *args, **kwargs):
         pass
 
     def do_GET(self):
-        req = self._validateHeaders()
-        self._auth()
+        req = self._createRequest()
+        if req is None:
+            return
 
         if self.path == '/crossdomain.xml':
             return self._handleResponse(self.serveCrossDomainFile())
-        if self.path == '/%s/clouds/ec2/images' % self.server.toplevel:
+        if self.path == '/%s/clouds/ec2/images' % self.toplevel:
             return self.enumerateImages(req)
-        if self.path == '/%s/clouds/ec2/instances' % self.server.toplevel:
+        if self.path == '/%s/clouds/ec2/instances' % self.toplevel:
             return self.enumerateInstances(req)
-        if self.path == '/%s/clouds/ec2/instanceTypes' % self.server.toplevel:
+        if self.path == '/%s/clouds/ec2/instanceTypes' % self.toplevel:
             return self.enumerateInstanceTypes(req)
-
+        p = '/%s/users/' % self.toplevel
+        if self.path.startswith(p):
+            return self._handleResponse(self.getUserData(req, self.path[len(p):]))
 
     def do_PUT(self):
-        req = self._validateHeaders()
-        self._auth()
+        req = self._createRequest()
+        if req is None:
+            return
 
     def do_POST(self):
-        req = self._validateHeaders()
-        self._auth()
+        req = self._createRequest()
+        if req is None:
+            return
+
+        p = '/%s/users/' % self.toplevel
+        if self.path.startswith(p):
+            self._handleResponse(self.addUserData(req, self.path[len(p):]))
 
     def do_DELETE(self):
+        req = self._createRequest()
+        if req is None:
+            return
+
+    def _createRequest(self):
         req = self._validateHeaders()
-        self._auth()
+        if req is None:
+            return None
+        if not self._auth(req):
+            return None
+        return req
 
     def _validateHeaders(self):
         if 'Host' not in self.headers:
@@ -128,13 +188,33 @@ class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             self.send_error(400)
             return
 
+        if 'Authorization' not in self.headers:
+            self._send_401()
+            return
+
         self.host = self.headers['Host']
         self.port = self.server.server_port
         req = StandaloneRequest(self)
         return req
 
-    def _auth(self):
-        pass
+    def _send_401(self):
+        self.send_response(401, "Unauthorized")
+        self.send_header('WWW-Authenticate', 'Basic realm="blah"')
+        self.send_header('Content-Type', 'text/html')
+        self.send_header('Connection', 'close')
+        self.end_headers()
+
+    def _auth(self, req):
+        authData = self.headers['Authorization']
+        if authData[:6] != 'Basic ':
+            self._send_401()
+            return False
+        authData = authData[6:]
+        authData = base64.decodestring(authData)
+        authData = authData.split(':', 1)
+        req.setUser(authData[0])
+        req.setPassword(authData[1])
+        return True
 
     def _handleResponse(self, response):
         response.addContentLength()
@@ -217,8 +297,36 @@ class BaseRESTHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         f = open(path)
         return Response(contentType = 'application/xml', fileObj = f)
 
+    def addUserData(self, req, userData):
+        # Split the arguments
+        userData = userData.split('/')
+        if userData[0] != req.getUser():
+            raise Exception("XXX 1")
+
+        dataLen = req.getContentLength()
+        data = req.read(dataLen)
+        store = storage.DiskStorage(self.storageConfig)
+
+        keyPrefix = '/'.join(x for x in userData if x not in ('', '.', '..'))
+
+        newId = store.store(data, keyPrefix = keyPrefix)
+        response = '<?xml version="1.0" encoding="UTF-8"?><id>%s/%s</id>' % (
+            req.getAbsoluteURI(), os.path.basename(newId))
+        return Response(contentType = "text/xml", data = response)
+
+    def getUserData(self, req, userData):
+        # Split the arguments
+        userData = userData.split('/')
+        if userData[0] != req.getUser():
+            raise Exception("XXX 1")
+
+        store = storage.DiskStorage(self.storageConfig)
+        key = '/'.join(x for x in userData if x not in ('', '.', '..'))
+        data = store.get(key)
+        return Response(contentType = "application/xml", data = data)
+
 class HTTPServer(BaseHTTPServer.HTTPServer):
-    toplevel = 'TOPLEVEL'
+    pass
 
 if __name__ == '__main__':
     h = HTTPServer(("", 1234), BaseRESTHandler)
---
+++ tired/storage.py
@@ -0,0 +1,193 @@
+#!/usr/bin/python2.4
+#
+# Copyright (c) 2008 rPath, Inc.
+#
+# This program is distributed under the terms of the Common Public License,
+# version 1.0. A copy of this license should have been distributed with this
+# source file in a file called LICENSE. If it is not present, the license
+# is always available at http://www.rpath.com/permanent/licenses/CPL-1.0.
+#
+# This program is distributed in the hope that it will be useful, but
+# without any warranty; without even the implied warranty of merchantability
+# or fitness for a particular purpose. See the Common Public License for
+# full details.
+#
+
+import os
+import sys
+
+from conary.lib import util
+
+#{ Exception classes
+class StorageError(Exception):
+    """Base class for all exceptions in the C{storage} module"""
+
+class InvalidKeyError(StorageError):
+    """An invalid key was specified"""
+
+class KeyNotFoundError(StorageError):
+    """The specified key was not found"""
+#}
+
+class BaseStorage(object):
+    """
+    Persistance class for the Package Creator
+    cvar separator: separator between the class prefix and the key
+    cvar keyLength: length of a key (not counting the optional prefix)
+    """
+    separator = ','
+    keyLength = 16
+
+    def __getitem__(self, key):
+        """Get the value for the specified key.
+        @param key: the key
+        @type key: C{str}
+        @rtype: C{str}
+        @return: The value for the key, if set
+        @raises InvalidKeyError: if the key was invalid
+        @raises KeyNotFoundError: if the key is not set
+        """
+        key = self._sanitizeKey(key)
+        if not self.exists(key):
+            raise KeyNotFoundError(key)
+        return self._real_get(key)
+
+    def __setitem__(self, key, val):
+        """Set the value for the specified key.
+        @param key: the key
+        @type key: C{str}
+        @param val: value to store
+        @type val: C{str}
+        @raises InvalidKeyError: if the key was invalid
+        """
+        key = self._sanitizeKey(key)
+        return self._real_set(key, val)
+
+    def set(self, key, val):
+        """Set the value for the specified key.
+        @param key: the key
+        @type key: C{str}
+        @param val: value to store
+        @type val: C{str}
+        @raises InvalidKeyError: if the key was invalid
+        """
+        return self.__setitem__(key, val)
+
+    def get(self, key, default = None):
+        """Get the value for the specified key.
+        @param key: the key
+        @type key: C{str}
+        @param default: a default value to be returned if the key is not set
+        @type default: C{str}
+        @rtype: C{str}
+        @return: The value for the key, if set, or the value of C{default}
+        @raises InvalidKeyError: if the key was invalid
+        """
+        if not self.exists(key):
+            return default
+        return self.__getitem__(key)
+
+    def exists(self, key):
+        """Check for a key's existance
+        @param key: the key
+        @type key: C{str}
+        @rtype: C{bool}
+        @return: True if the key exists, False otherwise
+        """
+        key = self._sanitizeKey(key)
+        return self._real_exists(key)
+
+    def store(self, val, keyPrefix = None):
+        """Generate a new key, and store the value.
+        @param val: value to store
+        @type val: C{str}
+        @param keyPrefix: a prefix to be prepended to the key
+        @type keyPrefix: C{str}
+        @return: The newly generated key
+        @rtype: C{str}
+        @raises StorageError: if the module was unable to generate a new key
+        """
+        for i in range(5):
+            newKey = self._generateString(self.keyLength)
+            if keyPrefix:
+                key = self.separator.join([keyPrefix, newKey])
+            else:
+                key = newKey
+            if not self.exists(key):
+                break
+        else: # for
+            raise StorageError("Failed to generate a new key")
+
+        self.set(key, val)
+        return key
+
+    #{ Methods that could be overwritten in subclasses
+    def _generateString(self, length):
+        """Generate a string
+        @param length: length of the string to be generated
+        @type length: C{int}
+        @rtype: C{int}
+        @return: The new string
+        """
+        randByteCount = int(round(length / 2.0))
+        bytes = file("/dev/urandom").read(randByteCount)
+        bytes = ''.join('%02x' % ord(x) for x in bytes)
+        return bytes[:length]
+    #}
+
+    #{ Methods that should be overwritten in subclasses
+    def _sanitizeKey(self, key):
+        """Sanitize the key by removing potentially dangerous characters.
+        @param key: key
+        @type key: C{str}
+        @rtype: C{str}
+        @return: The sanitized key
+        @raises InvalidKeyError: if the key was invalid
+        """
+
+    def _real_get(self, key):
+        raise NotImplementedError()
+
+    def _real_set(self, key, val):
+        raise NotImplementedError()
+
+    def _real_exists(self, key):
+        raise NotImplementedError()
+
+    #}
+
+class DiskStorage(BaseStorage):
+    separator = os.sep
+
+    def __init__(self, cfg):
+        """Constructor
+        @param cfg: Configuration object
+        @type cfg: C{config.PackageCreatorServiceConfiguration}
+        """
+        self.cfg = cfg
+
+    def _sanitizeKey(self, key):
+        nkey = os.path.normpath(key)
+        if key != nkey:
+            raise InvalidKeyError(key)
+        if key[0] == self.separator:
+            raise InvalidKeyError(key)
+        return key
+
+    def _real_get(self, key):
+        fpath = self._getFileForKey(key)
+        return file(fpath).read()
+
+    def _real_set(self, key, val):
+        fpath = self._getFileForKey(key, createDirs = True)
+        file(fpath, "w").write(str(val))
+
+    def _real_exists(self, key):
+        fpath = self._getFileForKey(key)
+        return os.path.exists(fpath)
+
+    def _getFileForKey(self, key, createDirs = False):
+        ret = self.separator.join([self.cfg.storagePath, key])
+        if createDirs:
+            util.mkdirChain(os.path.dirname(ret))
+        return ret
