437f6938a8f346dca047613f06ee0a411f5e3fe4
Stu Gott <http://issues.rpath.com/>
2008-07-18 11:04:51 -0400

    better URI determination (RBL-3134)

--- catalogService/handler_apache.py
+++ catalogService/handler_apache.py
@@ -43,17 +43,14 @@ class ApacheRequest(brequest.BaseRequest):
                              self._req.parsed_uri[apache.URI_FRAGMENT])
 
     def getSchemeNetloc(self):
-        scheme = self._req.parsed_uri[apache.URI_SCHEME]
-        scheme = scheme and scheme.lower() or 'http'
         via = self._req.headers_in.get('Via')
         if via and self._req.proxyreq:
+            # XXX We simply have to err on the side of encrypted requests...
+            scheme = 'https'
             hostport = via.split(',')[-1].split()[1]
         else:
-            port = self._req.parsed_uri[apache.URI_PORT]
-            hostport = self._req.parsed_uri[apache.URI_HOSTNAME]
-            knownPorts = set([('http', 80), ('https', 443)])
-            if (scheme, port) not in knownPorts:
-                hostport = "%s:%s" % (hostport, port)
+            scheme = self._req.is_https() and 'https' or 'http'
+            hostport = self._req.headers_in.get('Host')
         return "%s://%s" % (scheme, hostport, )
 
     def getHeader(self, key):
