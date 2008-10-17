ff51882387eb95f48bf67ab7e815b3e3185c073a
Mihai Ibanescu <https://issues.rpath.com/>
2008-10-17 14:03:46 -0400

    Reworked class hierarchy, changed No-store case

--- catalogService/rest/response.py
+++ catalogService/rest/response.py
@@ -2,16 +2,14 @@ from restlib import response
 
 from catalogService import xmlNode
 
-class XmlResponse(response.Response):
-    def __init__(self, content, *args, **kw):
-        hndlr = xmlNode.Handler()
-        newContent = hndlr.toXml(content)
-        response.Response.__init__(self, newContent, *args, **kw)
-        self.headers['content-type'] = 'application/xml'
-        self.headers['Cache-Control'] = 'No-store'
-
 class XmlStringResponse(response.Response):
     def __init__(self, *args, **kw):
         response.Response.__init__(self, *args, **kw)
         self.headers['content-type'] = 'application/xml'
-        self.headers['Cache-Control'] = 'No-store'
+        self.headers['Cache-Control'] = 'no-store'
+
+class XmlResponse(XmlStringResponse):
+    def __init__(self, content, *args, **kw):
+        hndlr = xmlNode.Handler()
+        newContent = hndlr.toXml(content)
+        XmlStringResponse.__init__(self, newContent, *args, **kw)
