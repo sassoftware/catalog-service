89cc4edc7ad2a84706ad0c336e4f9c2c94e2bf02
Mihai Ibanescu <https://issues.rpath.com/>
2008-10-28 11:25:23 -0400

    User credentials descriptor (RBL-3450)

--- catalogService/rest/drivers/xenent/xenentclient.py
+++ catalogService/rest/drivers/xenent/xenentclient.py
@@ -76,51 +76,37 @@ _configurationDescriptorXmlData = """<?xml version='1.0' encoding='UTF-8'?>
 _credentialsDescriptorXmlData = """<?xml version='1.0' encoding='UTF-8'?>
 <descriptor xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xsi:schemaLocation="http://www.rpath.org/permanent/descriptor-1.0.xsd descriptor-1.0.xsd">
   <metadata>
-    <displayName>Globus Workspaces User Credentials</displayName>
+    <displayName>Xen Enterprise User Credentials</displayName>
     <descriptions>
-      <desc>User Credentials for Globus Workspaces</desc>
+      <desc>User Credentials for Xen Enterprise</desc>
     </descriptions>
   </metadata>
   <dataFields>
     <field>
-      <name>userCert</name>
+      <name>username</name>
       <descriptions>
-        <desc>X509 User Certificate</desc>
+        <desc>User Name</desc>
       </descriptions>
       <type>str</type>
       <constraints>
         <descriptions>
           <desc>Maximum Characters</desc>
         </descriptions>
-        <length>4096</length>
+        <length>64</length>
       </constraints>
       <required>true</required>
     </field>
     <field>
-      <name>userKey</name>
+      <name>password</name>
       <descriptions>
-        <desc>X509 User Key</desc>
+        <desc>Password</desc>
       </descriptions>
       <type>str</type>
       <constraints>
         <descriptions>
           <desc>Maximum Characters</desc>
         </descriptions>
-        <length>4096</length>
-      </constraints>
-      <required>true</required>
-    </field>
-    <field>
-      <name>sshPubKey</name>
-      <descriptions>
-        <desc>SSH Public Key</desc>
-      </descriptions>
-      <type>str</type>
-      <constraints>
-        <descriptions>
-          <desc>Maximum Characters</desc>
-        </descriptions>
-        <length>4096</length>
+        <length>64</length>
       </constraints>
       <required>true</required>
     </field>
