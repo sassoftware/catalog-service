ca8aa4fcde6c646816806c2355ed3a141773c5ad
Mihai Ibanescu <https://issues.rpath.com/>
2010-06-04 16:02:12 -0400

    Attaching credentials disks to xen enterprise (RBL-4767)

--- catalogService/rest/drivers/xenent/xenentclient.py
+++ catalogService/rest/drivers/xenent/xenentclient.py
@@ -1,6 +1,8 @@
 
 import os
+import re
 import signal
+import select
 import socket
 import tempfile
 import time
@@ -129,7 +131,7 @@ class StreamingRequestMixIn(object):
             return parentClass._send_request(self, method, url, body,
                 headers)
         # Compute body length
-        pos = body.tell()
+        pos = 0
         body.seek(0, 2)
         contentLength = body.tell() - pos
         body.seek(pos)
@@ -137,11 +139,24 @@ class StreamingRequestMixIn(object):
         # Send the request with no body
         parentClass._send_request(self, method, url, None, headers)
         # Now stream the body
+        blockSize = 16384
+        pollobj = select.poll()
+        pollobj.register(self.sock, select.POLLOUT)
         while 1:
-            buf = body.read(16384)
+            buf = body.read(blockSize)
             if not buf:
                 break
-            self.send(buf)
+            pollobj.poll()
+            try:
+                self.send(buf)
+            except socket.error, e:
+#                import traceback; traceback.print_exc()
+                # Inject the response (headers may be interesting) in the
+                # socket error
+                e.response = self.getresponse()
+                e.response.close()
+                raise
+        print "Done"
 
 class HTTPConnection(httplib.HTTPConnection, StreamingRequestMixIn):
     def _send_request(self, method, url, body, headers):
@@ -194,8 +209,22 @@ class UploadClient(object):
         # Give it a chance to override Content-Type
         headers.update(headers or {})
         req = self.requestClass(self.url, data, headers = headers)
-        resp = self.getOpener().open(req)
-        return resp
+
+        for i in range(5):
+            try:
+                resp = self.getOpener().open(req)
+                return resp
+            except urllib2.URLError, e:
+                if hasattr(e.args[0], 'errno') and e.args[0].errno == 104:
+                    # Connection reset by peer. xen has the bad habit of
+                    # closing the request before one had the chance to read
+                    # the response
+                    print "   Failure", i, e.args[0].response.status
+                    continue
+                raise
+        else:
+            raise
+        return None
 
 class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
     Image = XenEnt_Image
@@ -213,6 +242,10 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
     XenSessionClass = None
 
     _XmlRpcWrapper = "<methodResponse><params><param>%s</param></params></methodResponse>"
+
+    minSizeRe = re.compile(".*VDI size must be between (.*) and .*|"
+        "VDI size must be a minimum of (.*)")
+
 
     @classmethod
     def isDriverFunctional(cls):
@@ -234,7 +267,16 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
         except XenAPI.Failure, e:
             raise errors.PermissionDenied(message = "User %s: %s" % (
                 e.details[1], e.details[2]))
+        self._uuidToRefMap = {}
         return sess
+
+    def _cachedGet(self, uuid, function):
+        ref = self._uuidToRefMap.get(uuid)
+        if ref is not None:
+            return ref
+        ref = function(uuid)
+        self._uuidToRefMap[uuid] = ref
+        return ref
 
     def drvVerifyCloudConfiguration(self, config):
         return
@@ -251,7 +293,7 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
             if len(x.getInstanceId()) == 36 ]
 
         for instId in realInstIds:
-            instRef = self.client.xenapi.VM.get_by_uuid(instId)
+            instRef = self._cachedGet(instId, client.xenapi.VM.get_by_uuid)
             client.xenapi.VM.clean_shutdown(instRef)
 
         self._killRunningProcessesForInstances(synthesizedInstIds)
@@ -337,7 +379,8 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
         return startTime
 
     def drvGetInstances(self, instanceIds):
-        instMap  = self.client.xenapi.VM.get_all_records()
+        client = self.client
+        instMap  = client.xenapi.VM.get_all_records()
         cloudAlias = self.getCloudAlias()
         instanceList = instances.BaseInstances()
         for opaqueId, vm in instMap.items():
@@ -348,7 +391,7 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
             publicIpAddr = None
             guestMetricsRef = vm['guest_metrics']
             if guestMetricsRef != 'OpaqueRef:NULL':
-                networks = self.client.xenapi.VM_guest_metrics.get_networks(
+                networks = client.xenapi.VM_guest_metrics.get_networks(
                     guestMetricsRef)
                 # XXX we are assuming eth0
                 publicIpAddr = networks.get('0/ip')
@@ -373,32 +416,23 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
         instanceList.sort(key = lambda x: (x.getState(), x.getInstanceId()))
         return self.filterInstances(instanceIds, instanceList)
 
-    def _putImage(self, vmFile, srUuid, taskRef):
-        srRef = self.client.xenapi.SR.get_by_uuid(srUuid)
-        urlTemplate = 'http://%s:%s@%s/import?task_id=%s&sr_id=%s'
-
-        cloudConfig = self.drvGetCloudConfiguration()
-        cloudName = cloudConfig['name']
-        creds = self.credentials
-        username, password = creds['username'], creds['password']
-        client = UploadClient(urlTemplate % (username, password, cloudName,
-            taskRef, srRef))
-        resp = client.request(file(vmFile))
-        # The server does not send any useful information back. Close the
-        # request and fetch the status from the task
-        resp.close()
-        for i in range(60):
-            task = self.client.xenapi.task.get_record(taskRef)
-            if task.get('status') == 'pending':
-                time.sleep(1)
+    def _pollTask(self, taskRef, loopCount = 100, timeout = 0.5):
+        client = self.client
+        for i in range(loopCount):
+            status = client.xenapi.task.get_status(taskRef)
+            # http://docs.vmd.citrix.com/XenServer/4.0.1/api/docs/html/browser.html
+            # Valid status: pending, success, failure, cancelling, cancelled
+            if status == 'pending':
+                time.sleep(timeout)
                 continue
-            if task.get('status') != 'success':
-                errorInfo = task.get('error_info', '')
-                raise errors.CatalogError("Unable to upload image %s: %s" %
-                    (vmFile, errorInfo))
-            break
-        else:
-            raise errors.CatalogError("Task is pending for too long")
+            return client.xenapi.task.get_record(taskRef)
+        return None
+
+    def _putVmImage(self, vmFile, srUuid, taskRef):
+        client = self.client
+        srRef = self._cachedGet(srUuid, client.xenapi.SR.get_by_uuid)
+        task = self._xePutFile('import', file(vmFile), taskRef,
+            loopCount = 10000, sr_id = srRef)
         # Wrap the pseudo-XMLRPC response
         params = XenAPI.xmlrpclib.loads(self._XmlRpcWrapper %
             task['result'])[0]
@@ -407,16 +441,129 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
             raise errors.CatalogError("Unable to publish image, no results found")
         vmRef = reflist[0]
         # Make it a template
-        self.client.xenapi.VM.set_is_a_template(vmRef, True)
+        client.xenapi.VM.set_is_a_template(vmRef, True)
 
-        vmUuid = self.client.xenapi.VM.get_uuid(vmRef)
+        vmUuid = client.xenapi.VM.get_uuid(vmRef)
         return vmRef, vmUuid
+
+    def _newVdi(self, vmUuid, fileObj, srRef):
+        client = self.client
+        fileObj.seek(0, 2)
+        fileSize = fileObj.tell()
+        fileObj.seek(0)
+
+        nameLabel = "Credentials image for %s" % vmUuid
+        vdiRef = self._createVdi(srRef, vmUuid, fileSize)
+
+        for i in range(10):
+            try:
+                taskRef = client.xenapi.task.create(nameLabel, nameLabel)
+                task = self._xePutFile('import_raw_vdi', fileObj,
+                    taskRef = taskRef, vdi = vdiRef)
+                break
+            except urllib2.URLError, e:
+                if hasattr(e.args[0], 'errno') and e.args[0].errno == 104:
+                    # Connection reset by peer. xen has the bad habit of
+                    # closing the request before one had the chance to read
+                    # the response
+                    print "Failure", i
+                    time.sleep(1)
+                    continue
+                raise
+        else: # for
+            raise
+
+        if task.get('status') != 'success':
+            errorInfo = task.get('error_info', '')
+            # Get rid of the vdi, it's not useful anymore
+            client.xenapi.VDI.destroy(vdiRef)
+            raise errors.CatalogError(
+                "Unable to upload initial credentials for %s: %s" %
+                    vmUuid, errorInfo)
+        return vdiRef
+
+    def _createVdi(self, srRef, vmUuid, fileSize):
+        client = self.client
+        vdiRec = {
+            'SR' : srRef,
+            'type' : 'system',
+            'virtual_size' : str(fileSize),
+            'name_label' : 'Credentials for %s' % vmUuid,
+            'name_description' : 'Credentials for %s' % vmUuid,
+            'sharable' : False,
+            'read_only' : True,
+            'other_config' : {},
+        }
+        try:
+            vdiRef = client.xenapi.VDI.create(vdiRec)
+        except XenAPI.Failure, e:
+            # This is disgusting. I could not find a minimal size for the SR,
+            # so for now we're parsing the error.
+            if e.details[0] != 'SR_BACKEND_FAILURE_79':
+                raise
+            m = self.minSizeRe.match(e.details[2])
+            if not m:
+                raise
+            matchedGroups = m.groups()
+            matched = matchedGroups[0]
+            if matched is None:
+                matched = matchedGroups[1]
+            arr = matched.split()
+            dsize = int(arr[0])
+            if len(arr) >= 2:
+                # Is there a multiplier?
+                mult = arr[1].upper()
+                if mult == 'MB':
+                    multiplier = 1024 * 1024
+                elif mult == 'KB':
+                    multiplier = 1024
+                elif mult == 'GB':
+                    multiplier = 1024 * 1024 * 1024
+                else:
+                    multiplier = 1
+                dsize = multiplier * dsize
+            vdiRec['virtual_size'] = str(dsize)
+            vdiRef = client.xenapi.VDI.create(vdiRec)
+        return vdiRef
+
+    def _xePutFile(self, urlSelector, fileObj, taskRef, loopCount = 1000,
+            loopTimeout = 0.5, **kwargs):
+        urlTemplate = 'http://%s:%s@%s/%s?%s'
+        cloudConfig = self.drvGetCloudConfiguration()
+        cloudName = cloudConfig['name']
+        creds = self.credentials
+        username, password = creds['username'], creds['password']
+        kwargs['task_id'] = taskRef
+
+        query = '&'.join("%s=%s" % (k, kwargs[k]) for k in sorted(kwargs))
+
+        #print ("curl -T /tmp/foo.iso '%s'" % (urlTemplate % (username, password, cloudName, urlSelector, query)))
+        client = UploadClient(urlTemplate % (username, password, cloudName,
+            urlSelector, query))
+        resp = client.request(fileObj)
+        # The server does not send any useful information back. Close the
+        # request and fetch the status from the task
+        if resp:
+            resp.close()
+        rec = self._pollTask(taskRef, loopCount, timeout = loopTimeout)
+        if not rec:
+            # timeout
+            raise errors.CatalogError("Failure uploading file")
+        status = rec.get('status')
+        if status == 'success':
+            return rec
+        if status == 'failure':
+            errorInfo = rec['error_info']
+            raise errors.CatalogError("Unable to upload file: %s" %
+                    (errorInfo, ))
+        # Canceled
+        raise errors.CatalogError("Task has finished unexpectedly: %s" % status)
 
     def _importImage(self, image, vmFile, srUuid):
         checksum = image.getImageId()
         taskRef = self.client.xenapi.task.create("Import of %s" % checksum,
             "Import of %s" % checksum)
-        vmRef, vmUuid = self._putImage(vmFile, srUuid, taskRef)
+        vmRef, vmUuid = self._putVmImage(vmFile, srUuid, taskRef)
         self._setVmMetadata(vmRef, checksum = checksum)
         return vmRef, vmUuid
 
@@ -463,6 +610,11 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
         job.addLog(self.LogEntry('Cloning template'))
         realId = self.cloneTemplate(job, imageId, instanceName,
             instanceDescription)
+        job.addLog(self.LogEntry('Attaching credentials'))
+        try:
+            self._attachCredentials(realId, srUuid)
+        except Exception, e:
+            self.log_exception("Exception attaching credentials: %s" % e)
         job.addLog(self.LogEntry('Launching'))
         self.startVm(realId)
         return realId
@@ -516,29 +668,101 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
             self._instanceStore.delete(instId)
 
     def cloneTemplate(self, job, imageId, instanceName, instanceDescription):
-        vmTemplateRef = self.client.xenapi.VM.get_by_uuid(imageId)
+        client = self.client
+        vmTemplateRef = self._cachedGet(imageId, client.xenapi.VM.get_by_uuid)
         imageId = os.path.basename(imageId)
 
-        vmRef = self.client.xenapi.VM.clone(vmTemplateRef,
+        vmRef = client.xenapi.VM.clone(vmTemplateRef,
             instanceName)
-        self.client.xenapi.VM.set_name_description(vmRef, instanceDescription)
+        client.xenapi.VM.set_name_description(vmRef, instanceDescription)
         self._setVmMetadata(vmRef, templateUuid = imageId)
 
         # Get all physical interfaces
-        pifs = self.client.xenapi.PIF.get_all_records()
+        pifs = client.xenapi.PIF.get_all_records()
         # Grab the lowest device (eth0)
         lowest = min((v['device'], k) for k, v in pifs.items())[1]
 
-        networkRef = self.client.xenapi.PIF.get_network(lowest)
+        networkRef = client.xenapi.PIF.get_network(lowest)
 
         self.addVIFtoVM(vmRef, networkRef)
+
+        # Generate credentials image here
         return vmRef
 
+    def _attachCredentials(self, vmRef, srUuid):
+        client = self.client
+        srRef = self._cachedGet(srUuid, client.SR.get_by_uuid)
+        filename = self.getCredentialsIsoFile()
+        fileObj = file(filename)
+        os.unlink(filename)
+        self._attachCredentialsDisk(vmRef, fileObj, srRef)
+
+    def _attachCredentialsDisk(self, vmRef, fileObj, srRef = None):
+        client = self.client
+        if srRef is None:
+            srRef = self._getSrFromVmRef(vmRef)
+        vmUuid = client.xenapi.VM.get_uuid(vmRef)
+        vdiRef = self._newVdi(vmUuid, fileObj, srRef)
+        try:
+            vbdRef = self._newVbd(vmRef, vdiRef)
+        except:
+            client.xenapi.VDI.destroy(vdiRef)
+            raise
+        #try:
+        #    self._plugVbd(vbdRef)
+        #except:
+        #    exc_info = sys.exc_info()
+        #    try:
+        #        client.xenapi.VDI.destroy(vdiRef)
+        #        client.xenapi.VBD.destroy(vbdRef)
+        #    except:
+        #        pass
+        #    raise exc_info[0], exc_info[1], exc_info[2]
+
+    def _newVbd(self, vmRef, vdiRef, vbdType="disk"):
+        client = self.client
+        # Determine available devices
+        allowedVbds = client.xenapi.VM.get_allowed_VBD_devices(vmRef)
+        # Grab the first one
+        vbdDevNo = allowedVbds[0]
+        # Create the vbd
+        vbdRec = dict(
+            userdevice = vbdDevNo,
+            VM = vmRef,
+            VDI = vdiRef,
+            type = vbdType,
+            mode = "RO",
+            bootable = False,
+            empty = False,
+            other_config = {},
+            qos_algorithm_type = '',
+            qos_algorithm_params = {},
+        )
+        vbdRef = client.xenapi.VBD.create(vbdRec)
+        return vbdRef
+
+    def _plugVbd(self, vbdRef):
+        ret = self.client.xenapi.VBD.plug(vbdRef)
+        return ret
+
+    def _getSrFromVmRef(self, vmRef):
+        # For VMs that we just pushed, we should not do this, we already know
+        # the SR
+        client = self.client
+        # Grab the VBDs first
+        for vbdRef in client.xenapi.VM.get_VBDs(vmRef):
+            # Grab VDIs for this vbd
+            vdiRef = client.xenapi.VBD.get_VDI(vbdRef)
+            srRef = client.xenapi.VDI.get_SR(vdiRef)
+            return srRef
+        return None
+
     def startVm(self, vmRef):
-        self.client.xenapi.VM.provision(vmRef)
+        client = self.client
+        client.xenapi.VM.provision(vmRef)
         startPaused = False
         force = False
-        self.client.xenapi.VM.start(vmRef, startPaused, force)
+        client.xenapi.VM.start(vmRef, startPaused, force)
 
     def addVIFtoVM(self, vmRef, networkRef):
         vifRec = {
@@ -576,14 +800,15 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
 
     def _getStorageRepos(self):
         # Get all pools
-        pools = self.client.xenapi.pool.get_all_records()
+        client = self.client
+        pools = client.xenapi.pool.get_all_records()
         srList = [ x['default_SR'] for x in pools.values() ]
         # Validate the sr list
         uuidsFound = dict()
         ret = []
         for srRef in srList:
             try:
-                uuid = self.client.xenapi.SR.get_uuid(srRef)
+                uuid = client.xenapi.SR.get_uuid(srRef)
                 if uuid in uuidsFound:
                     continue
                 ret.append(uuid)
@@ -594,7 +819,7 @@ class XenEntClient(storage_mixin.StorageMixin, baseDriver.BaseDriver):
 
         hCache = XenEntHostCache(self.client)
 
-        srRecs = self.client.xenapi.SR.get_all_records()
+        srRecs = client.xenapi.SR.get_all_records()
         for k, srRec in sorted(srRecs.items(), key = lambda x: x[1]['uuid']):
             uuid = srRec['uuid']
             if not srRec['PBDs']:
