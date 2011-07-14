b6194d3741807d83222f49378aee4f250b79a6b1
Mihai Ibanescu <https://issues.rpath.com/>
2011-07-14 13:37:27 -0400

    Properly look for xml nodes in OVF 1.0

--- catalogService/libs/viclient/client.py
+++ catalogService/libs/viclient/client.py
@@ -1360,7 +1360,8 @@ class OVF(object):
             sections = self.getNetworkSections() or []
             ret = []
             for section in sections:
-                for network in OVF._getNodeByName(section, 'Network', self.xmlns):
+                for network in OVF._getNodeByName(section, 'Network',
+                        [self.ovfNs, None]):
                     name = OVF._getNodeAttribute(network, 'name', self.ovfNs)
                     if name:
                         ret.append(name)
@@ -1375,7 +1376,7 @@ class OVF(object):
         def addHardwareItems(self, hardwareSection, nextInstanceId):
             networkNames = self.getNetworkNames()
             # Add back ethernet0 as E1000
-            item = hardwareSection.makeelement("Item")
+            item = hardwareSection.makeelement("{%s}Item" % self.ovfNs)
             hardwareSection.append(item)
             if networkNames:
                 networkName = networkNames[0]
@@ -1390,12 +1391,12 @@ class OVF(object):
 
         def getNetworkSections(self):
             return OVF._getSectionsByType(self.doc, 'NetworkSection_Type',
-                self.xmlns, self.xsiNs)
+                self.ovfNs, self.xsiNs)
 
         def getHardwareSections(self):
             contentNode = self.doc.find(OVF._xmltag('Content', self.xmlns))
             return OVF._getSectionsByType(contentNode,
-                'VirtualHardwareSection_Type', self.xmlns, self.xsiNs)
+                'VirtualHardwareSection_Type', self.ovfNs, self.xsiNs)
 
         def _addNetworkFields(self, item, nextInstanceId, networkName):
             self.addNode(item, self.elementNameTag, "ethernet0")
@@ -1413,11 +1414,11 @@ class OVF(object):
         elementNameTag = "ElementName"
 
         def getNetworkSections(self):
-            return self.doc.findall(OVF._xmltag('NetworkSection', self.xmlns))
+            return self.doc.findall(OVF._xmltag('NetworkSection', self.ovfNs))
 
         def getHardwareSections(self):
-            vsys = self.doc.find(OVF._xmltag('VirtualSystem', self.xmlns))
-            return vsys.findall(OVF._xmltag('VirtualHardwareSection', self.xmlns))
+            vsys = self.doc.find(OVF._xmltag('VirtualSystem', self.ovfNs))
+            return vsys.findall(OVF._xmltag('VirtualHardwareSection', self.ovfNs))
 
         def _addNetworkFields(self, item, nextInstanceId, networkName):
             self.addNode(item, "AddressOnParent", "7")
@@ -1437,6 +1438,9 @@ class OVF(object):
         doc = etree.fromstring(string)
         for cls in self.OvfVersions:
             if self._getNsPrefix(doc, cls.ovfNs):
+                # In case the default ns is None, set it to the proper one
+                if None not in doc.nsmap:
+                    doc.nsmap[None] = cls.ovfNs
                 self.ovf = cls(doc)
                 break
         else: # for
@@ -1457,9 +1461,12 @@ class OVF(object):
         # instanceId is supposed to be unique, so compute the max; we'll add
         # our new devices starting with this max
         maxInstanceId = 0
-        itemTag = self._xmltag('Item', self.ovf.xmlns)
+        itemTags = set([ self._xmltag('Item', self.ovf.ovfNs)])
+        if self.ovf.xmlns is None:
+            # Need to look for an item in the default namespace too
+            itemTags.add(self._xmltag('Item', None))
         for i, node in enumerate(hardwareSection.iterchildren()):
-            if node.tag != itemTag:
+            if node.tag not in itemTags:
                 continue
 
             resourceTypeText = self.ovf.getNodeText(node, "ResourceType")
@@ -1494,8 +1501,14 @@ class OVF(object):
         return ret
 
     @classmethod
-    def _getNodeByName(cls, node, name, xmlns):
-        return node.findall(cls._xmltag(name, xmlns)) or []
+    def _getNodeByName(cls, node, name, xmlnsList):
+        if not isinstance(xmlnsList, (list, tuple)):
+            xmlnsList = [ xmlnsList ]
+        for xmlns in xmlnsList:
+            ret = node.findall(cls._xmltag(name, xmlns))
+            if ret:
+                return ret
+        return []
 
     @classmethod
     def _getSectionsByType(cls, node, xstype, xmlns, xsins):
@@ -1503,6 +1516,9 @@ class OVF(object):
         # than ovf:, this would stop working.
         xstype = "ovf:" + xstype
         typeAttrib = cls._xmltag("type", xsins)
+        if xmlns is not None:
+            # Look for the node in the default namespace too
+            xmlns = (xmlns, None)
         sections = cls._getNodeByName(node, 'Section', xmlns)
         return [ x for x in sections if x.get(typeAttrib) == xstype ]
 
